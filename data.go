5fa62cf4dcb6b28a30535072c99a43204931d7f8
hoeg <peter.steffensen@gmail.com>
2021-02-06 14:41:45 +0100

    added util for fields

---
+++ data.go
@@ -0,0 +1,15 @@
+package dockerhub
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/vault/sdk/framework"
+)
+
+func getStringFrom(data *framework.FieldData, key string) string {
+	v := data.Get(key)
+	if s, ok := v.(string); ok || s != "" {
+		return s
+	}
+	panic(fmt.Sprintf("no string from %s", key))
+}
