2b99197ac1869d15df8664e9c0f995969d3d40b3
Peter Hoeg Steffensen <peter.steffensen@gmail.com>
2024-01-20 21:33:00 +0000

    added testing code

--- internal/dockerhub/client_test.go
+++ internal/dockerhub/client_test.go
@@ -19,11 +19,29 @@ func TestCreateToken(t *testing.T) {
 		Conf: &conf,
 	}
 
-	token, err := client.NewToken(context.Background(), "test", "hoeg")
+	token, err := client.NewToken(context.Background(), "tes1", "hoeg")
 	if err != nil {
 		t.Fatal(err)
 	}
 	if token.UUID == "" {
 		t.Fatal("no valid token returned")
+	}
+}
+
+func TestDeleteToken(t *testing.T) {
+	t.Skip()
+	conf := config.Config{
+		Namespace: []string{"hoeg"},
+		Username:  "hoeg",
+		Password:  "",
+	}
+
+	client := Client{
+		Conf: &conf,
+	}
+
+	err := client.DeleteToken(context.Background(), "c071fa97-fe94-41d5-b1f8-96af5604e3d3", "hoeg")
+	if err != nil {
+		t.Fatal(err)
 	}
 }
